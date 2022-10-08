dcf0c4c437681ff123e6ada033f6786e5bc33034
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-08 12:00:39 -0300

    Mudando Exercicio 01, Adicionando para contar também as vogais com acentos

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
+///////////Exercicio 01///////////
 let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"));
 ContarVogais(Pergunta);
 function ContarVogais(str) {
     let TamanhoPalavra = str.length;
-    let Resultado = TamanhoPalavra - str.replace(/[aeiou]/gi, "").length;
+    let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi, "").length;
     console.log(Resultado);
 }
+///////////Exercicio 02///////////
--- script.ts
+++ script.ts
@@ -1,13 +1,19 @@
 
 
+///////////Exercicio 01///////////
+
 let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"))
 
 ContarVogais(Pergunta)
 
-
-
 function ContarVogais(str: string){
     let TamanhoPalavra = str.length
-    let Resultado = TamanhoPalavra - str.replace(/[aeiou]/gi,"").length
+    let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi,"").length
     console.log(Resultado)
 }
+
+
+
+
+
+///////////Exercicio 02///////////
