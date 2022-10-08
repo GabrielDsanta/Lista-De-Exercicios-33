1d9730f1526ee5100c5d41672dc48232859ca6e0
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-08 17:17:14 -0300

    Exercicio 03 Finalizado

--- script.js
+++ script.js
@@ -8,15 +8,33 @@
 //     console.log(Resultado)
 // }
 ///////////Exercicio 02///////////
-let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"));
-AcharMeioPalavra(Pergunta);
-function AcharMeioPalavra(str) {
-    if (str.length % 2 == 0) {
-        console.log(str[str.length / 2 - 1] + str[str.length / 2]);
+// let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"))
+// AcharMeioPalavra(Pergunta)
+// function AcharMeioPalavra(str: string){
+//     if(str.length % 2 == 0){
+//         console.log(str[str.length / 2 - 1] + str[str.length / 2])
+//     }
+//     else{
+//         let ListaDeLetras = Pergunta.split("")
+//         ListaDeLetras.shift()
+//         console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1])
+//     }
+// }
+///////////Exercicio 02///////////
+let Pergunta = String(prompt("Digite um número para ordena-lo !"));
+OrdernarNumeros(Pergunta);
+function OrdernarNumeros(Pergunta) {
+    let str = "";
+    let ListaDeNumeros = Pergunta.split("");
+    for (let index = 0; index < 10; index++) {
+        for (let i = 0; i <= index; i++) {
+            if (ListaDeNumeros[i] == index) {
+                ListaDeNumeros[i] = +str;
+                ListaDeNumeros[i] = index;
+            }
+        }
     }
-    else {
-        let ListaDeLetras = Pergunta.split("");
-        ListaDeLetras.shift();
-        console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1]);
-    }
+    ListaDeNumeros.sort();
+    ListaDeNumeros.reverse();
+    console.log(ListaDeNumeros);
 }
--- script.ts
+++ script.ts
@@ -14,21 +14,53 @@
 
 
 
+
+
 ///////////Exercicio 02///////////
 
 
-let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"))
+// let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"))
 
-AcharMeioPalavra(Pergunta)
+// AcharMeioPalavra(Pergunta)
 
-function AcharMeioPalavra(str: string){
-    if(str.length % 2 == 0){
-        console.log(str[str.length / 2 - 1] + str[str.length / 2])
+// function AcharMeioPalavra(str: string){
+//     if(str.length % 2 == 0){
+//         console.log(str[str.length / 2 - 1] + str[str.length / 2])
+//     }
+
+//     else{
+//         let ListaDeLetras = Pergunta.split("")
+//         ListaDeLetras.shift()
+//         console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1])
+//     }
+// }
+
+
+
+
+
+///////////Exercicio 02///////////
+
+let Pergunta = String(prompt("Digite um número para ordena-lo !"))
+
+OrdernarNumeros(Pergunta)
+
+
+function OrdernarNumeros(Pergunta){
+    let str: string = ""
+    let ListaDeNumeros = Pergunta.split("")
+
+    for (let index = 0; index < 10; index++){
+        for (let i = 0; i <= index; i++) {
+            if(ListaDeNumeros[i] == index){
+                ListaDeNumeros[i] = +str
+                ListaDeNumeros[i] = index
+            }
+        }
     }
 
-    else{
-        let ListaDeLetras = Pergunta.split("")
-        ListaDeLetras.shift()
-        console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1])
-    }
+    ListaDeNumeros.sort()
+    ListaDeNumeros.reverse()
+
+    console.log(ListaDeNumeros)
 }
