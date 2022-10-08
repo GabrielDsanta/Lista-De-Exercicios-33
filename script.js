f2988fe494d160d7ca99aa2958b7dc5030870adf
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-08 12:47:48 -0300

    Exercicio 02 Finalizado

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 "use strict";
 ///////////Exercicio 01///////////
-let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"));
-ContarVogais(Pergunta);
-function ContarVogais(str) {
-    let TamanhoPalavra = str.length;
-    let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi, "").length;
-    console.log(Resultado);
-}
+// let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"))
+// ContarVogais(Pergunta)
+// function ContarVogais(str: string){
+//     let TamanhoPalavra = str.length
+//     let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi,"").length
+//     console.log(Resultado)
+// }
 ///////////Exercicio 02///////////
+let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"));
+AcharMeioPalavra(Pergunta);
+function AcharMeioPalavra(str) {
+    if (str.length % 2 == 0) {
+        console.log(str[str.length / 2 - 1] + str[str.length / 2]);
+    }
+    else {
+        let ListaDeLetras = Pergunta.split("");
+        ListaDeLetras.shift();
+        console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1]);
+    }
+}
--- script.ts
+++ script.ts
@@ -2,18 +2,33 @@
 
 ///////////Exercicio 01///////////
 
-let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"))
+// let Pergunta = String(prompt("Digite uma palavra e veja quantas vogais possui nela !"))
 
-ContarVogais(Pergunta)
+// ContarVogais(Pergunta)
 
-function ContarVogais(str: string){
-    let TamanhoPalavra = str.length
-    let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi,"").length
-    console.log(Resultado)
-}
-
-
+// function ContarVogais(str: string){
+//     let TamanhoPalavra = str.length
+//     let Resultado = TamanhoPalavra - str.replace(/[aeiou à-ú]/gi,"").length
+//     console.log(Resultado)
+// }
 
 
 
 ///////////Exercicio 02///////////
+
+
+let Pergunta = String(prompt("Digite uma palavra e veja o meio dela !"))
+
+AcharMeioPalavra(Pergunta)
+
+function AcharMeioPalavra(str: string){
+    if(str.length % 2 == 0){
+        console.log(str[str.length / 2 - 1] + str[str.length / 2])
+    }
+
+    else{
+        let ListaDeLetras = Pergunta.split("")
+        ListaDeLetras.shift()
+        console.log(ListaDeLetras[ListaDeLetras.length / 2 - 1])
+    }
+}
